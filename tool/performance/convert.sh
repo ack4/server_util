fcaebad2945390267c2741e778d1d12c532a2f8a
sample <sample@sample>
2018-07-04 17:15:39 +0900

    mod convert.sh->append util

--- tool/performance/convert.sh
+++ tool/performance/convert.sh
@@ -2,13 +2,28 @@
 BASE_DIR=$(dirname $(readlink -f $0))
 EVI_DIR=${1-$(find ${BASE_DIR} -maxdepth 1 -type d  -name 'EVIDENCE_*' | sort -r | head -1)}
 
-RESULT_FREE=${EVI_DIR}/result_free
 RESULT_VMSTAT=${EVI_DIR}/result_vmstat
+RESULT_FREE=${EVI_DIR}/result_free
 RESULT_IOSTAT=${EVI_DIR}/result_iostat
+
+function conv_vmstat()
+{
+    declare local HEADER="datetime,r,b,swpd,free,buff,cache,si,so,bi,bo,in,cs,us,sy,id,wa,st,util"
+    echo "${HEADER}"
+    cat - \
+        | tr -s ' ' \
+        | grep -v '^$' \
+        | grep -v '^procs' \
+        | grep -v '^START' \
+        | sed 's/^ *//g' \
+        | tr ' ' ',' \
+        | grep -v '^r,' \
+        | awk -F, 'BEGIN {OFS=","} {print $18 " " $19,$1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$13+$14+$16+$17}'
+}
 
 function conv_free()
 {
-    declare local HEADER="date,time,total,used,free,shared,buff_cache,available,swap_total,swap_userd,swap_free"
+    declare local HEADER="datetime,total,used,free,shared,buff_cache,available,swap_total,swap_userd,swap_free,util"
     echo "${HEADER}"
     cat - \
         | tr -s ' ' \
@@ -22,12 +37,13 @@ function conv_free()
         | sed 's/@@@/\n/g' \
         | sed -E 's/^,(.*),$/\1/g' \
         | grep -v '^START' \
-        | grep -v '^,'
+        | grep -v '^,' \
+        | awk -F, 'BEGIN {OFS=",";OFMT="%.1f"} {print $1 " " $2,$3,$4,$5,$6,$7,$8,$9,$10,$11,($3-$8)/$3*100}'
 }
 
 function conv_iostat()
 {
-    declare local HEADER="date,time,AM_PM,Device,rrqm/s,wrqm/s,r/s,w/s,rMB/s,wMB/s,avgrq-sz,avgqu-sz,await,r_await,w_await,svctm,util"
+    declare local HEADER="datetime,Device,rrqm/s,wrqm/s,r/s,w/s,rMB/s,wMB/s,avgrq-sz,avgqu-sz,await,r_await,w_await,svctm,util"
     echo "${HEADER}"
     cat - \
         | tr -s ' ' \
@@ -37,23 +53,18 @@ function conv_iostat()
         | grep -v '^Linux' \
         | sed -E 's/^(..)\/(..)\/(....) (..:..:.. ..)/\n\3-\1-\2 \4/g' \
         | awk 'BEGIN {FS="\n";RS="";OFS="\n"} {print $1 " " $2,$1 " " $3,$1 " " $4}' \
-        | tr ' ' ','
+        | tr ' ' ',' \
+        | awk -F, 'BEGIN {OFS=","} {print $1 " " $2 " " $3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17}' \
+        | while read LINE
+          do
+              declare local TMP=$(echo ${LINE} | cut -d, -f2-15)
+              declare local DATETIME_AMPM=$(echo ${LINE} | cut -d, -f1)
+              #AM,PM表記を変換
+              DATETIME_AMPM=$(date -d "${DATETIME_AMPM}" +"%F %T")
+              echo "${DATETIME_AMPM},${TMP}"
+          done
 }
 
-function conv_vmstat()
-{
-    declare local HEADER="date,time,r,b,swpd,free,buff,cache,si,so,bi,bo,in,cs,us,sy,id,wa,st"
-    echo "${HEADER}"
-    cat - \
-        | tr -s ' ' \
-        | grep -v '^$' \
-        | grep -v '^procs' \
-        | grep -v '^START' \
-        | sed 's/^ *//g' \
-        | tr ' ' ',' \
-        | grep -v '^r,' \
-        | awk -F, 'BEGIN {OFS=","} {print $18,$19,$1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17}'
-}
 
 #check resutl file
 if [ ! -f "${RESULT_FREE}" -o ! -f "${RESULT_VMSTAT}" -o ! -e "${RESULT_IOSTAT}" ]; then
@@ -61,8 +72,8 @@ if [ ! -f "${RESULT_FREE}" -o ! -f "${RESULT_VMSTAT}" -o ! -e "${RESULT_IOSTAT}"
   exit 99
 fi
 
-cat ${RESULT_FREE} | conv_free > ${EVI_DIR}/result_conv_free.csv
 cat ${RESULT_VMSTAT} | conv_vmstat > ${EVI_DIR}/result_conv_vmstat.csv
+cat ${RESULT_FREE} | conv_free > ${EVI_DIR}/result_conv_free.csv
 cat ${RESULT_IOSTAT} | conv_iostat > ${EVI_DIR}/result_conv_iostat.csv
 
 
