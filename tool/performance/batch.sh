1d2accbf734241faa556ec9acc30da9072b841ba
t30010 <t30010@04-7554>
2018-10-16 12:41:08 +0900

    rename and little modify

--- tool/performance/batch.sh
+++ tool/performance/batch.sh
@@ -1,11 +1,13 @@
-#!/bin/bash
+#!/bin/bash -u
 BASE_DIR=$(dirname $(readlink -f $0))
 
-find ${BASE_DIR} -type d -name 'EVIDENCE_*' | xargs -I{} ${BASE_DIR}/convert.sh {}
+find ${BASE_DIR} -type d -name 'EVIDENCE_*' | xargs -I{} ${BASE_DIR}/2_convert_old.sh {}
 find ${BASE_DIR} -name 'EVIDENCE_*' -type d | sort | while read LINE
 do
-    echo "${LINE}" | xargs -I{} ${BASE_DIR}/calc.sh {}
+    echo "${LINE}" | xargs -I{} ${BASE_DIR}/3_calc.sh {} | tee "${LINE}/calc_result.tsv"
     echo
 done
+
+${BASE_DIR}/4_evidence.sh
 
 exit 0
